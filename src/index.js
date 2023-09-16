29c41dab9ac91c9fc391f7bf18f42b36d9e63674
prudrakshula <prudrakshula@nouveau-labs.com>
2023-09-16 14:43:18 +0530

    babel-css-scss configured

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('Hello from webpack');
+import "./index.scss";
+
+const foo = (name) => {
+  console.log(`My name is ${name}`);
+};
+
+foo('Prasanth');
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ]
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+        title: "Html Plugin title"
+      })
+    ]
+};
